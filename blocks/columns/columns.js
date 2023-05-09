1a158a9ca2fb15693ec22c0a625d8e071660741e
Sagar <sadduri@adobe.com>
2023-05-09 12:02:02 +0530

    combined all download a links to one

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,3 +1,30 @@
+function joinLinkstoOne(links) {
+  if (links.length > 1 && links.every((link) => link.getAttribute('href') === links[0].getAttribute('href'))) {
+    const span = document.createElement('span');
+    const firstChild = links[0];
+    const { parentElement } = firstChild;
+
+    // Remove the first <a> element from the links array
+    links.shift();
+
+    // Move all child nodes of the <a> elements to the new <span> element
+    links.forEach((link) => {
+      if (link.parentNode.tagName === 'EM') {
+        const emTag = document.createElement('em');
+        emTag.innerHTML = link.innerHTML;
+        span.innerHTML += emTag.outerHTML;
+      } else {
+        span.innerHTML += link.innerHTML;
+      }
+    });
+
+    firstChild.appendChild(span);
+
+    // Replace the original parent element with the new <a> element
+    parentElement.innerHTML = firstChild.outerHTML;
+  }
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -13,6 +40,15 @@ export default function decorate(block) {
           picWrapper.classList.add('columns-img-col');
         }
       }
+
+      // combine individual 'a' links to single 'a' link
+      const links = Array.from(col.querySelectorAll('div>a,div>em>a'));
+      joinLinkstoOne(links);
+      const pTags = Array.from(col.querySelectorAll('div>p'));
+      pTags.forEach((pTag) => {
+        const pLinks = Array.from(pTag.querySelectorAll('a,em>a'));
+        joinLinkstoOne(pLinks);
+      });
     });
   });
 }
